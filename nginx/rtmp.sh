c44bc9667672979ec6806b0a61acc42f44685bbe
taoky <me@taoky.moe>
2022-10-29 23:46:35 +0800

    nginx: use docker for rtmp instead of aur package 'nginx-mod-rtmp'

--- nginx/rtmp.sh
+++ nginx/rtmp.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-nginx -c "$PWD"/rtmp.conf
+# nginx -c "$PWD"/rtmp.conf
+
+# Use docker image tiangolo/nginx-rtmp here, instead of nginx-mod-rtmp on AUR
+docker run --rm --network=host --name nginx-rtmp -v "$PWD"/rtmp.conf:/etc/nginx/nginx.conf tiangolo/nginx-rtmp
+
