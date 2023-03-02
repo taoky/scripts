e86dfbd9ed4fc8113268e5ae648618fff5fe859d
taoky <me@taoky.moe>
2023-03-02 20:46:33 +0800

    use ssh -O exit in cleaning ssh control

--- web/clean-ssh-control.sh
+++ web/clean-ssh-control.sh
@@ -1,4 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 
-rm -v ~/.ssh/control-*
+shopt -s globstar nullglob
+for i in ~/.ssh/control-*; do
+    name=$(basename "$i")
+    name=${name#"control-"}
+    IFS=":" read -r -a name_arr <<< "$name"
+    host=${name_arr[0]}
+    port=${name_arr[1]}
+    echo "Closing $host:$port"
+    ssh -O exit -p "$port" "$host"; 
+done
 
