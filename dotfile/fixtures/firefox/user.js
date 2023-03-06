b421afebc6fd822c7b773caae1c0e95ec2b5e284
taoky <me@taoky.moe>
2023-03-06 17:19:11 +0800

    add firefox user.js

---
+++ dotfile/fixtures/firefox/user.js
@@ -0,0 +1,6 @@
+// Copy this file to ~/.mozilla/firefox/<profile>/
+
+user_pref("devtools.toolbox.zoomValue", "1.5");
+user_pref("font.name.monospace.x-western", "JetBrains Mono");
+
+user_pref("middlemouse.paste", false);
