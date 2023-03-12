cf67fe0bc1d1e917b831d9547b5b5f8b2dce1993
taoky <me@taoky.moe>
2023-03-13 00:49:36 +0800

    firefox: enable hardware acceleration

--- dotfile/fixtures/firefox/user.js
+++ dotfile/fixtures/firefox/user.js
@@ -4,3 +4,6 @@ user_pref("devtools.toolbox.zoomValue", "1.5");
 user_pref("font.name.monospace.x-western", "JetBrains Mono");
 
 user_pref("middlemouse.paste", false);
+
+user_pref("media.ffmpeg.vaapi.enabled", true);
+
