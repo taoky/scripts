0e66bef3daffd71e32e14a836aeb9006a7b499c9
taoky <me@taoky.moe>
2023-03-26 05:41:33 +0800

    ip_count: faster impl without ipaddress module

--- network/ip_count.py
+++ network/ip_count.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python3
 # zcat access.log.gz | awk '{print $1}' | python ip_count.py
-import ipaddress
 import sys
 from collections import defaultdict
+from socket import inet_pton, inet_ntop, AF_INET, AF_INET6
 
 IPV4_CIDR = 24
 IPV6_CIDR = 48
+IPV4_MAX = 32
+IPV6_MAX = 128
 
 
 def get_ip_prefix(ip: str) -> str:
     if ":" in ip:
-        # IPv6
-        suffix = f"/{IPV6_CIDR}"
+        typ = AF_INET6
+        suffix = IPV6_CIDR
+        shift = IPV6_MAX - IPV6_CIDR
+        bytelen = 16
     else:
-        suffix = f"/{IPV4_CIDR}"
-    network = ipaddress.ip_network(ip + suffix, strict=False)
-    address = str(network.network_address) + suffix
+        typ = AF_INET
+        suffix = IPV4_CIDR
+        shift = IPV4_MAX - IPV4_CIDR
+        bytelen = 4
+    ip = inet_pton(typ, ip)
+    ip = int.from_bytes(ip, "big", signed=False) >> shift << shift
+    address = inet_ntop(typ, int.to_bytes(ip, length=bytelen, byteorder="big")) + f"/{suffix}"
     return address
 
 
