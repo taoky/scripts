fc890f92fac1ded17ab907c5de0fa742ff75754a
taoky <me@taoky.moe>
2023-04-10 21:50:50 +0800

    Update config

--- dotfile/etc/sync.sh
+++ dotfile/etc/sync.sh
@@ -3,3 +3,5 @@
 rsync -a /etc/pacman.conf pacman.conf
 rsync -a /etc/pacman.d/mirrorlist pacman.d/mirrorlist
 rsync -a /etc/system76-scheduler/ system76-scheduler
+rsync -a /etc/systemd/system/bluetooth-disable-before-sleep.service systemd/system/
+rsync -a /etc/environment .
