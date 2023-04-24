142e356d251c61ad64cb713dc7aa92ff9aa5b7e7
taoky <me@taoky.moe>
2023-04-25 07:18:01 +0800

    working on gstreaming

--- x11docker/wemeet/run.sh
+++ x11docker/wemeet/run.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-x11docker --gpu=virgl -I --xephyr --clipboard --desktop --pulseaudio=host --webcam --sudouser -i local/wemeet-x11
+# Line 4031 Westonoutput="WL"
+x11docker --gpu -I --weston-xwayland --westonini=$(pwd)/weston.ini --home --clipboard --desktop --pulseaudio=host --webcam --sudouser -i local/wemeet-x11
