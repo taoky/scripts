335dab022416e787590fc76830185984086f0949
taoky <me@taoky.moe>
2023-04-27 22:08:04 +0800

    add current system76-scheduler user config

--- dotfile/etc/sync.sh
+++ dotfile/etc/sync.sh
@@ -2,7 +2,7 @@
 
 rsync -a /etc/pacman.conf pacman.conf
 rsync -a /etc/pacman.d/mirrorlist pacman.d/mirrorlist
-# rsync -a /etc/system76-scheduler/ system76-scheduler
+rsync -a /etc/system76-scheduler/ system76-scheduler
 rsync -a /etc/systemd/system/bluetooth-disable-before-sleep.service systemd/system/
 rsync -a /etc/systemd/system/intel-gpu-frequency.service systemd/system/
 rsync -a /etc/environment .
