6ab06ba59ef1f566f12a25fe30033a5d0210bf93
taoky <me@taoky.moe>
2023-05-04 14:35:35 +0800

    add /usr/lib/rustup/bin/ to fish path

--- dotfile/.config/fish/conf.d/user-env.fish
+++ dotfile/.config/fish/conf.d/user-env.fish
@@ -5,3 +5,7 @@ set -x TLDR_CACHE_MAX_AGE 1680
 # User appended path
 fish_add_path ~/.cargo/bin
 fish_add_path ~/.local/bin
+
+# Arch Linux rustup package after 2023/05/04
+fish_add_path /usr/lib/rustup/bin
+
