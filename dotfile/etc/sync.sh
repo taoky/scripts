d7fbf8edae0bf61cfeee7f0e34e7da20c3fd1a2e
taoky <me@taoky.moe>
2023-06-13 17:40:19 +0800

    Add files for kdump

--- dotfile/etc/sync.sh
+++ dotfile/etc/sync.sh
@@ -5,4 +5,7 @@ rsync -a /etc/pacman.d/mirrorlist pacman.d/mirrorlist
 rsync -a /etc/system76-scheduler/ system76-scheduler
 rsync -a /etc/systemd/system/bluetooth-disable-before-sleep.service systemd/system/
 rsync -a /etc/systemd/system/intel-gpu-frequency.service systemd/system/
+rsync -a /etc/systemd/system/kdump*.service systemd/system/
+rsync -a /etc/mkinitcpio* .
 rsync -a /etc/environment .
+rsync -a /etc/default/grub default/
