5d73d8638006ae582864d68a99747ab8cad6901b
taoky <me@taoky.moe>
2023-06-27 01:36:06 +0800

    update fish and tmux config

--- dotfile/.config/fish/conf.d/10-user-env.fish
+++ dotfile/.config/fish/conf.d/10-user-env.fish
@@ -9,3 +9,6 @@ fish_add_path ~/.local/bin
 # Arch Linux rustup package after 2023/05/04
 fish_add_path /usr/lib/rustup/bin
 
+# "If you installed Nix from the official repositories, you must add the ~/.nix-profile/bin directory to your PATH manually."
+fish_add_path ~/.nix-profile/bin
+
