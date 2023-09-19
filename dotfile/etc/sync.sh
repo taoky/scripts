7ea519c825c32ca813710f17bebb3a579fc1158e
taoky <me@taoky.moe>
2023-09-19 13:08:35 +0800

    update config

--- dotfile/etc/sync.sh
+++ dotfile/etc/sync.sh
@@ -2,6 +2,7 @@
 
 rsync -a /etc/pacman.conf pacman.conf
 rsync -a /etc/pacman.d/mirrorlist pacman.d/mirrorlist
+rsync -a /etc/pacman.d/hooks/ pacman.d/hooks
 rsync -a /etc/system76-scheduler/ system76-scheduler
 rsync -a /etc/systemd/system/bluetooth-disable-before-sleep.service systemd/system/
 rsync -a /etc/systemd/system/intel-gpu-frequency.service systemd/system/
