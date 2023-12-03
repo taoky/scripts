da04232a1531e1476e282842633d45fc5f3c2b46
taoky <me@taoky.moe>
2023-12-03 23:56:40 +0800

    update config

--- dotfile/.config/fish/conf.d/10-user-env.fish
+++ dotfile/.config/fish/conf.d/10-user-env.fish
@@ -2,6 +2,7 @@ set -x PIP_REQUIRE_VIRTUALENV true
 set -x EDITOR vim
 set -x SYSTEMD_EDITOR vim
 set -x TLDR_CACHE_MAX_AGE 1680
+set -x JQ_COLORS "1;30:0;39:0;39:0;39:0;32:1;39:1;39"
 
 # User appended path
 fish_add_path ~/.cargo/bin
