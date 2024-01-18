dcb4844d69a4b6e43db9ade97c119bca1d860c3a
taoky <me@taoky.moe>
2024-01-18 16:53:41 +0800

    update config

--- dotfile/etc/sync.sh
+++ dotfile/etc/sync.sh
@@ -2,16 +2,16 @@
 
 rsync -a /etc/pacman.conf pacman.conf
 rsync -a /etc/pacman.d/mirrorlist pacman.d/mirrorlist
-rsync -a /etc/pacman.d/hooks/ pacman.d/hooks
-rsync -a /etc/system76-scheduler/ system76-scheduler
+rsync -a --delete /etc/pacman.d/hooks/ pacman.d/hooks
+rsync -a --delete /etc/system76-scheduler/ system76-scheduler
 rsync -a /etc/systemd/system/bluetooth-disable-before-sleep.service systemd/system/
 rsync -a /etc/systemd/system/intel-gpu-frequency.service systemd/system/
 rsync -a /etc/systemd/system/kdump*.service systemd/system/
-rsync -a /etc/mkinitcpio* .
+rsync -a --delete /etc/mkinitcpio* .
 rsync -a /etc/environment .
 rsync -a /etc/default/grub default/
-rsync -a /etc/tlp.d/ tlp.d
-rsync -a /etc/modules-load.d/ modules-load.d
+rsync -a --delete /etc/tlp.d/ tlp.d
+rsync -a --delete /etc/modules-load.d/ modules-load.d
 rsync -a /etc/docker/daemon.json docker/daemon.json
-sudo rsync -a /etc/sudoers.d .
+sudo rsync -a --delete /etc/sudoers.d .
 sudo chown taoky:taoky -R ./sudoers.d
