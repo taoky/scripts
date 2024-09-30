3e1bb38c49b5fbc318a7f19b24395717dcf270b3
taoky <me@taoky.moe>
2024-09-30 15:11:33 +0800

    thunderbird: Update user.js

--- dotfile/fixtures/thunderbird/user.js
+++ dotfile/fixtures/thunderbird/user.js
@@ -1,3 +1,5 @@
 // ~/.thunderbird/<profile>/user.js
 
+user_pref("mail.server.default.allow_utf8_accept", false);
 user_pref("mailnews.wraplength", 0);
+user_pref("widget.use-xdg-desktop-portal.file-picker", 1);
