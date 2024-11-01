5c99f1bffcf794b7fecf27df298415adc51c0b19
taoky <me@taoky.moe>
2024-11-02 03:37:29 +0800

    x11: Use Arch's Xwayland

--- x11/wemeet/start.sh
+++ x11/wemeet/start.sh
@@ -1,9 +1,8 @@
 #!/bin/sh -e
 
 # Start xwayland
-# https://aur.archlinux.org/packages/xwayland-standalone-with-libdecor
 echo "Starting Xwayland"
-Xwayland-standalone :114 -ac -retro +extension RANDR +extension RENDER +extension GLX +extension XVideo +extension DOUBLE-BUFFER +extension SECURITY +extension DAMAGE +extension X-Resource -extension XINERAMA -xinerama +extension Composite +extension COMPOSITE -extension XTEST -tst -dpms -s off -decorate -geometry 1920x1080 &
+Xwayland :114 -ac -retro +extension RANDR +extension RENDER +extension GLX +extension XVideo +extension DOUBLE-BUFFER +extension SECURITY +extension DAMAGE +extension X-Resource -extension XINERAMA -xinerama +extension Composite +extension COMPOSITE -extension XTEST -tst -dpms -s off -decorate -geometry 1920x1080 &
 
 echo "Waiting for X server to be ready"
 while ! xdpyinfo -display :114 >/dev/null 2>&1; do
