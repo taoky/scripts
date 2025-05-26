73b7821cab5cc9eabe923f59b822e86cc8f74151
taoky <me@taoky.moe>
2025-05-27 04:37:56 +0800

    Update llmkey and wireplumber config

--- bin/llmkey.py
+++ bin/llmkey.py
@@ -3,11 +3,17 @@
 
 import keyring
 import openai
+from typing import TypeVar, Type
+import inspect
+import asyncio
 
-def get_ai(provider: str = "siliconflow") -> openai.OpenAI:
+T = TypeVar("T", openai.OpenAI, openai.AsyncOpenAI)
+
+
+async def _get_ai(client_cls: Type[T], provider: str) -> openai.OpenAI:
     key = keyring.get_password("llmkey", provider)
     if provider == "siliconflow":
-        ai = openai.OpenAI(base_url="https://api.siliconflow.cn/v1", api_key="placeholder")
+        ai = client_cls(base_url="https://api.siliconflow.cn/v1", api_key="placeholder")
     else:
         raise ValueError(f"Unknown provider: {provider}")
     if key is None:
@@ -17,7 +23,9 @@ def get_ai(provider: str = "siliconflow") -> openai.OpenAI:
     while True:
         ai.api_key = key
         try:
-            ai.models.list()
+            maybe_coro = ai.models.list()
+            if inspect.isawaitable(maybe_coro):
+                await maybe_coro
             # key ok, save it
             keyring.set_password("llmkey", provider, key)
             return ai
@@ -30,3 +38,16 @@ def get_ai(provider: str = "siliconflow") -> openai.OpenAI:
                 retries -= 1
             else:
                 raise
+
+
+def get_ai(provider: str = "siliconflow") -> openai.OpenAI:
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        return asyncio.run(_get_ai(openai.OpenAI, provider))
+    else:
+        return loop.run_until_complete(_get_ai(openai.AsyncOpenAI, provider))
+
+
+async def aget_ai(provider: str = "siliconflow") -> openai.AsyncOpenAI:
+    return await _get_ai(openai.AsyncOpenAI, provider)
