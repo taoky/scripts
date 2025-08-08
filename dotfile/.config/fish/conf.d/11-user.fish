4bc0141947b50e6cc6fe83e8a9be6e98431468b7
taoky <me@taoky.moe>
2025-08-08 23:53:51 +0800

    Add tmpxctr

--- dotfile/.config/fish/conf.d/11-user.fish
+++ dotfile/.config/fish/conf.d/11-user.fish
@@ -4,6 +4,7 @@ end
 
 alias kubectl="minikube kubectl --"
 alias tmpctr="podman run -it --rm --log-driver none"
+alias tmpxctr="podman run -it --rm --log-driver none -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTHORITY -v /tmp/.X11-unix:/tmp/.X11-unix -v $XAUTHORITY:$XAUTHORITY"
 alias vim="nvim"
 alias ccplay="flatpak run --file-forwarding moe.taoky.clicking-circles-player @@ ~/Projects/clicking-circles-player/song.json @@ ~/.var/app/sh.ppy.osu/data/osu/files/"
 
