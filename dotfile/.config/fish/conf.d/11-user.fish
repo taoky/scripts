a6a257859361d01d7c2b5953b7627109e77a6084
taoky <me@taoky.moe>
2025-10-15 03:33:25 +0800

    Remove nvim; Add dev container

--- dotfile/.config/fish/conf.d/11-user.fish
+++ dotfile/.config/fish/conf.d/11-user.fish
@@ -1,12 +1,15 @@
 if status is-login; and status is-interactive
-    /usr/bin/chafa --scale 0.4 /opt/motd.png
+    if test -f /opt/motd.png
+        /usr/bin/chafa --scale 0.4 /opt/motd.png
+    else
+        /usr/bin/chafa --scale 0.4 /opt/motd.jpg
+    end
 end
 
-alias kubectl="minikube kubectl --"
 alias tmpctr="podman run -it --rm --log-driver none"
 alias tmpxctr="podman run -it --rm --log-driver none -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTHORITY -v /tmp/.X11-unix:/tmp/.X11-unix -v $XAUTHORITY:$XAUTHORITY"
-alias vim="nvim"
 alias ccplay="flatpak run --file-forwarding moe.taoky.clicking-circles-player @@ ~/Projects/clicking-circles-player/song.json @@ ~/.var/app/sh.ppy.osu/data/osu/files/"
+alias dev="tmpctr -v $(pwd):$(pwd) -w $(pwd) local/dev-debian:13"
 
 function klogg
     if test -z "$argv"
