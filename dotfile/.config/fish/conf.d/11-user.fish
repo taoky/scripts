97cec8b073aa318a6d0581696f6df4696083fb45
taoky <me@taoky.moe>
2025-10-18 17:18:06 +0800

    Update dev dockerfile

--- dotfile/.config/fish/conf.d/11-user.fish
+++ dotfile/.config/fish/conf.d/11-user.fish
@@ -9,7 +9,7 @@ end
 alias tmpctr="podman run -it --rm --log-driver none"
 alias tmpxctr="podman run -it --rm --log-driver none -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTHORITY -v /tmp/.X11-unix:/tmp/.X11-unix -v $XAUTHORITY:$XAUTHORITY"
 alias ccplay="flatpak run --file-forwarding moe.taoky.clicking-circles-player @@ ~/Projects/clicking-circles-player/song.json @@ ~/.var/app/sh.ppy.osu/data/osu/files/"
-alias dev="tmpctr -v $(pwd):$(pwd) -w $(pwd) local/dev-debian:13"
+alias dev='tmpctr -v $(pwd):$(pwd) -w $(pwd) local/dev-debian:13'
 
 function klogg
     if test -z "$argv"
