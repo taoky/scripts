b0985d8782fbf64505f7c273676998f70d3626a8
taoky <me@taoky.moe>
2025-10-25 17:18:45 +0800

    add gptoss20b

--- dotfile/.config/fish/conf.d/11-user.fish
+++ dotfile/.config/fish/conf.d/11-user.fish
@@ -6,10 +6,16 @@ if status is-login; and status is-interactive
     end
 end
 
+set host (cat /etc/hostname)
+
 alias tmpctr="podman run -it --rm --log-driver none"
 alias tmpxctr="podman run -it --rm --log-driver none -e DISPLAY=$DISPLAY -e XAUTHORITY=$XAUTHORITY -v /tmp/.X11-unix:/tmp/.X11-unix -v $XAUTHORITY:$XAUTHORITY"
 alias ccplay="flatpak run --file-forwarding moe.taoky.clicking-circles-player @@ ~/Projects/clicking-circles-player/song.json @@ ~/.var/app/sh.ppy.osu/data/osu/files/"
 alias dev='tmpctr -v $(pwd):$(pwd) -w $(pwd) local/dev-debian:13'
+
+if test $host = "nanoka.taoky.moe"
+    alias gptoss20b='HIP_VISIBLE_DEVICES=0 llama-server -hf ggml-org/gpt-oss-20b-GGUF --ctx-size 32768 --jinja -ub 2048 -b 2048'
+end
 
 function klogg
     if test -z "$argv"
