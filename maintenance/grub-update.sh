5976194f114694f807a19b85169e2eb7ce1431f4
taoky <me@taoky.moe>
2025-12-14 23:53:51 +0800

    Update after LUKS

--- maintenance/grub-update.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh -e
-
-sudo mount /dev/disk/by-uuid/0E62-46C6 /efi
-sudo grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
-sudo grub-mkconfig -o /boot/grub/grub.cfg
-sudo umount /efi
