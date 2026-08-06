0ac343030f857487fc8c3e1fa7501b3be55954f6
taoky <me@taoky.moe>
2026-08-07 02:31:45 +0800

    Commit current changes

--- dotfile/.local/share/nautilus-python/extensions/open-with.py
+++ dotfile/.local/share/nautilus-python/extensions/open-with.py
@@ -33,12 +33,6 @@ class OpenWithExtension(GObject.GObject, Nautilus.MenuProvider):
                                      tip=_(u'Open this file or directory in Code'))
             item_code.connect('activate', self._code_callback, file_)
             items.append(item_code)
-            if file_.is_directory():
-                item_terminal = Nautilus.MenuItem(name='NautilusPython::open_in_terminal',
-                                                label=_(u'Open in Terminal'),
-                                                tip=_(u'Open this file or directory in Terminal'))
-                item_terminal.connect('activate', self._terminal_callback, file_)
-                items.append(item_terminal)            
         return items
 
     def get_background_items(self, *args):
@@ -50,18 +44,9 @@ class OpenWithExtension(GObject.GObject, Nautilus.MenuProvider):
                                      label=_(u'Open Code Here'),
                                      tip=_(u'Open current directory in Code'))
             item_code.connect('activate', self._code_callback, file_)
-            item_terminal = Nautilus.MenuItem(name='NautilusPython::open_terminal_bg',
-                                                label=_(u'Open Terminal Here'),
-                                                tip=_(u'Open current directory in Terminal'))
-            item_terminal.connect('activate', self._terminal_callback, file_)
             items.append(item_code)
-            items.append(item_terminal)
         return items
 
     def _code_callback(self, menu, file_):
         filename = Gio.File.new_for_uri(file_.get_uri()).get_path()
         spawn_nonblocking(['code', filename])
-    
-    def _terminal_callback(self, menu, file_):
-        filename = Gio.File.new_for_uri(file_.get_uri()).get_path()
-        spawn_nonblocking(['xdg-terminal-exec', f'--dir={shlex.quote(filename)}', '--', 'fish'])
